ec903341b6697b1bbaceb33b65063f669429e108
EagleoutIce <maeusepiep99@web.de>
2021-05-06 23:35:58 +0200

    fix ghost start for macos guard

--- core/src/main/java/de/flojo/core/StartGuards.java
+++ core/src/main/java/de/flojo/core/StartGuards.java
@@ -3,6 +3,7 @@ package de.flojo.core;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class StartGuards {
@@ -10,7 +11,7 @@ public class StartGuards {
 
     public static final String MACOS_GUARD = "-guard-macos";
 
-    // true if restartet
+    // true if restarted
     public static boolean guardMacOs(String[] args) {
         if(OperatingSystem.getOperatingSystemType().equals(OperatingSystemType.MacOS) && !hasGuard(args, MACOS_GUARD)) {
             try {
@@ -31,9 +32,17 @@ public class StartGuards {
                 .getCodeSource().getLocation()
                 .toURI().getPath()
                 .replace('/', File.separator.charAt(0));
-        final var command = "java " + extraPre + " -jar \""+ currentPath +"\" " + String.join(" ", args) + " " + extraArg;
-        System.out.println("Starting with: " +command);
-        Runtime.getRuntime().exec(command);
+        final var command = new ArrayList<String>();
+        command.add("java");
+        if(!extraPre.isBlank())
+            command.add(extraPre);
+        command.add("-jar");
+        command.add(currentPath);
+        command.addAll(Arrays.asList(args));
+        command.add(extraArg);
+        System.out.println("Starting with: " + command);
+        final var processBuilder = new ProcessBuilder(command);
+        processBuilder.start();
     }
 
     private static boolean hasGuard(String[] args, String guard) {
